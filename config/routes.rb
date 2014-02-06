1ee92afb2dd10269b5dcb9bbf8b4f12735f0d9c9
Basti Himmler <sebastian@himmler-tta.de>
2014-02-06 10:31:26 +0100

    Change devise routes from devise_for to devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Fox::Application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'users/registrations' } do
+  devise_for:users, :controllers => { registrations: 'users/registrations' }
+  devise_scope :user do
     get 'users/posts' => 'users/registrations#posts'
   end
 
